49fcaa4d0f64f8031d91be3db7b7cb55ae6f6363
Heidi Engstrom <hengstrom25@gmail.com>
2019-03-04 19:16:21 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,4 +1,5 @@
 // Code SimpleComponent Here
+<<<<<<< HEAD
 import React, {Component} from 'react';
   
 export default class SimpleComponent extends Component {
@@ -22,3 +23,16 @@ export default class SimpleComponent extends Component {
   }
   
 }
+=======
+
+class SimpleComponent {
+  
+  const mood = happy
+  
+  render () {
+    return (
+      )
+  }
+  
+}
+>>>>>>> 4cff3e8cf7b4a32233c546e11339e9dff8917e74
